f85c6ee6cda3ba75642f434671f0b00e0c9ea223
el-kg <kirillgalkin8@gmail.com>
2023-08-06 23:18:42 +0700

    Сделано ДЗ

---
+++ src/main/java/skypro/EmployeeBook/controller/EmployeeController.java
@@ -0,0 +1,40 @@
+package skypro.EmployeeBook.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import skypro.EmployeeBook.Employee;
+import skypro.EmployeeBook.service.EmployeeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.deleteEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.findEmployee(firstName, lastName);
+    }
+
+    @GetMapping
+    public List<Employee> getAll() {
+        return employeeService.findAll();
+    }
+}
---
+++ src/main/java/skypro/EmployeeBook/exception/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package skypro.EmployeeBook.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Такой сотрудник не найден!")
+public class EmployeeNotFoundException extends RuntimeException {
+
+}
